4bd8a4fa3e0555b8364bcb28e829ac003c7bca09
Florian Gessner <flo.gessner@gmail.com>
2017-05-08 18:44:21 +0200

    Change base package

--- src/main/java/de/gessnerfl/fakesmtp/FakeSmtpServerApplication.java
+++ src/main/java/de/gessnerfl/fakesmtp/FakeSmtpServerApplication.java
@@ -1,4 +1,4 @@
-package de.gessnerfl;
+package de.gessnerfl.fakesmtp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/de/gessnerfl/fakesmtp/FakeSmtpServerApplicationTests.java
+++ src/test/java/de/gessnerfl/fakesmtp/FakeSmtpServerApplicationTests.java
@@ -1,4 +1,4 @@
-package de.gessnerfl;
+package de.gessnerfl.fakesmtp;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
