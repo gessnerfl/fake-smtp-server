b46a2428c26dd7b96eaeb397b72e886f7a34a2c3
Florian Gessner <flo.gessner@gmail.com>
2017-05-14 21:24:26 +0200

    #3: fix docker build script

--- deploy-to-docker.sh
+++ deploy-to-docker.sh
@@ -7,5 +7,5 @@ docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
 export REPO=gessnerfl/fake-smtp-server
 
 docker build -f Dockerfile -t $REPO:$TRAVIS_TAG .
-docker tag $REPO:$COMMIT $REPO:latest
+docker tag $REPO:$TRAVIS_TAG $REPO:latest
 docker push $REPO
