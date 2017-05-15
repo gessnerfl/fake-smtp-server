9bc41e15c109a975a21e0c52d270b1c62de61d94
Florian Gessner <flo.gessner@gmail.com>
2017-05-15 18:39:06 +0200

    #3: fix docker build script and travis build configuration

--- deploy-to-docker.sh
+++ deploy-to-docker.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+echo "Copy deployment artifact"
 cp build/libs/fake-smtp-server-$TRAVIS_TAG.jar build/libs/fake-smtp-server.jar
 
+echo "Check docker version"
+docker --version
+
+echo "Login to dockerhub"
 docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
 
 export REPO=gessnerfl/fake-smtp-server
 
+echo "Build docker image"
 docker build -f Dockerfile -t $REPO:$TRAVIS_TAG .
+echo "Tag docker images as latest"
 docker tag $REPO:$TRAVIS_TAG $REPO:latest
+echo "Push to dockerhub"
 docker push $REPO
