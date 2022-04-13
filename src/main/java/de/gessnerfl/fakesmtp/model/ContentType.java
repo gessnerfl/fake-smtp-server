4388c46023fd83b156ab4be813c3f96aa2ba8e8c
FejfarKamil <55243705+FejfarKamil@users.noreply.github.com>
2022-04-13 18:40:54 +0200

    Update ContentType.java
    Content-type type/subtype should be case insensitive.

--- src/main/java/de/gessnerfl/fakesmtp/model/ContentType.java
+++ src/main/java/de/gessnerfl/fakesmtp/model/ContentType.java
@@ -1,9 +1,12 @@
 package de.gessnerfl.fakesmtp.model;
 
+import java.util.Locale;
+
 public enum ContentType {
     HTML, PLAIN, MULTIPART_ALTERNATIVE, MULTIPART_MIXED, MULTIPART_RELATED, UNDEFINED;
 
     public static ContentType fromString(String string) {
+        string = string.toLowerCase(Locale.ENGLISH);
         if (string.startsWith("multipart/alternative")) return MULTIPART_ALTERNATIVE;
         if (string.startsWith("multipart/mixed")) return MULTIPART_MIXED;
         if (string.startsWith("multipart/related")) return MULTIPART_RELATED;
