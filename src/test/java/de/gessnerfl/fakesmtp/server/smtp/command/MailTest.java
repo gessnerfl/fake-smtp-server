94cdec235f81b18556ad2a99781b6d0d21cad394
Florian Gessner <flo.gessner@gmail.com>
2022-12-27 22:23:47 +0100

    113: cleanup MailTest

--- src/test/java/de/gessnerfl/fakesmtp/server/smtp/command/MailTest.java
+++ src/test/java/de/gessnerfl/fakesmtp/server/smtp/command/MailTest.java
@@ -2,6 +2,8 @@ package de.gessnerfl.fakesmtp.server.smtp.command;
 
 import org.junit.jupiter.api.Test;
 import de.gessnerfl.fakesmtp.server.smtp.util.ServerTestCase;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class MailTest extends ServerTestCase {
 
@@ -27,8 +29,15 @@ class MailTest extends ServerTestCase {
 		this.expect("503 5.5.1 Sender already specified.");
 	}
 
-	@Test
-	void testInvalidSenders() throws Exception {
+	@ParameterizedTest
+	@CsvSource({
+			"MAIL FROM: <test@lkjsd lkjk>, 553 <test@lkjsd lkjk> Invalid email address.",
+			"MAIL, 501 Syntax: MAIL FROM: <address>  Error in parameters:",
+			"MAIL FROM: <>, 250",
+			"MAIL FROM:, 501 Syntax: MAIL FROM: <address>",
+			"MAIL FROM:<validuser@subethamail.org>, 250 Ok"
+	})
+	void testMailCommands(final String command, final String expectedResponse) throws Exception {
 		this.expect("220");
 
 		this.send("HELO foo.com");
@@ -36,87 +45,24 @@ class MailTest extends ServerTestCase {
 
 		// added <> because without them "lkjk" is a parameter
 		// to the MAIL command. (Postfix responds accordingly)
-		this.send("MAIL FROM: <test@lkjsd lkjk>");
-		this.expect("553 <test@lkjsd lkjk> Invalid email address.");
+		this.send(command);
+		this.expect(expectedResponse);
 	}
 
-	@Test
-	void testMalformedMailCommand() throws Exception {
-		this.expect("220");
-
-		this.send("HELO foo.com");
-		this.expect("250");
-
-		this.send("MAIL");
-		this.expect("501 Syntax: MAIL FROM: <address>  Error in parameters:");
-	}
-
-	@Test
-	void testEmptyFromCommand() throws Exception {
-		this.expect("220");
-
-		this.send("HELO foo.com");
-		this.expect("250");
-
-		this.send("MAIL FROM: <>");
-		this.expect("250");
-	}
-
-	@Test
-	void testEmptyEmailFromCommand() throws Exception {
-		this.expect("220");
-
-		this.send("HELO foo.com");
-		this.expect("250");
-
-		this.send("MAIL FROM:");
-		this.expect("501 Syntax: MAIL FROM: <address>");
-	}
-
-	@Test
-	void testMailWithoutWhitespace() throws Exception {
-		this.expect("220");
-
-		this.send("HELO foo.com");
-		this.expect("250");
-
-		this.send("MAIL FROM:<validuser@subethamail.org>");
-		this.expect("250 Ok");
-	}
-
-	@Test
-	void testSize() throws Exception {
+	@ParameterizedTest
+	@CsvSource({
+			"MAIL FROM:<validuser@subethamail.org> SIZE=100, 250 Ok",
+			"MAIL FROM:<validuser@subethamail.org>, 250 Ok",
+			"MAIL FROM:<validuser@subethamail.org> SIZE=1001, 552"
+	})
+	void testSizes(final String command, final String expectedResponse) throws Exception {
 		this.wiser.getServer().setMaxMessageSize(1000);
 		this.expect("220");
 
 		this.send("EHLO foo.com");
 		this.expectContains("250-SIZE 1000");
 
-		this.send("MAIL FROM:<validuser@subethamail.org> SIZE=100");
-		this.expect("250 Ok");
-	}
-
-	@Test
-	void testSizeWithoutSize() throws Exception {
-		this.wiser.getServer().setMaxMessageSize(1000);
-		this.expect("220");
-
-		this.send("EHLO foo.com");
-		this.expectContains("250-SIZE 1000");
-
-		this.send("MAIL FROM:<validuser@subethamail.org>");
-		this.expect("250 Ok");
-	}
-
-	@Test
-	void testSizeTooLarge() throws Exception {
-		this.wiser.getServer().setMaxMessageSize(1000);
-		this.expect("220");
-
-		this.send("EHLO foo.com");
-		this.expectContains("250-SIZE 1000");
-
-		this.send("MAIL FROM:<validuser@subethamail.org> SIZE=1001");
-		this.expect("552");
+		this.send(command);
+		this.expect(expectedResponse);
 	}
 }
