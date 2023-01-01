9eb340e20094a78758a47f1f6a144d1b5e16ad44
Florian Gessner <flo.gessner@gmail.com>
2023-01-01 20:50:19 +0100

    113: remove unused imports

--- src/main/java/de/gessnerfl/fakesmtp/smtp/server/BaseSmtpServer.java
+++ src/main/java/de/gessnerfl/fakesmtp/smtp/server/BaseSmtpServer.java
@@ -6,8 +6,6 @@ import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 import javax.net.ssl.SSLSocket;
 import javax.net.ssl.SSLSocketFactory;
