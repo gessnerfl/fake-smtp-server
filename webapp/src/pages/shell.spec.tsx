3c2f3227bbf79ceae14cad5f60ca14d3c86a25da
Florian Gessner <flo.gessner@gmail.com>
2023-04-06 15:02:24 +0200

    122: add assertion

--- webapp/src/pages/shell.spec.tsx
+++ webapp/src/pages/shell.spec.tsx
@@ -1,9 +1,13 @@
 import * as React from 'react'
+import '@testing-library/jest-dom'
 import {render, screen} from '@testing-library/react'
 import Shell from "./shell";
 
 describe('Shell', () => {
     it('render shell component', () => {
         render(<Shell />);
+
+        //Check for Navigation bar; Content not rendered as we check the element not in combination with router
+        expect(screen.getByText("FakeSMTPServer")).toBeInTheDocument()
     })
 })
