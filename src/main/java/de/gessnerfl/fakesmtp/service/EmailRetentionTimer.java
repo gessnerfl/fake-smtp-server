86516f8eaa647d0b5ab0f84b0eddb77a9e18f5cd
vilherda <vilherda@gmail.com>
2024-07-17 16:15:42 +0200

    Update EmailRetentionTimer.java
    On this way these elements are configurable

--- src/main/java/de/gessnerfl/fakesmtp/service/EmailRetentionTimer.java
+++ src/main/java/de/gessnerfl/fakesmtp/service/EmailRetentionTimer.java
@@ -24,7 +24,7 @@ public class EmailRetentionTimer {
         this.logger = logger;
     }
 
-    @Scheduled(fixedDelay = 300000, initialDelay = 60000)
+    @Scheduled(fixedDelay = ${fakesmtp.persistence.fixedDelay:300000}, initialDelay = ${fakesmtp.persistence.initialDelay:60000})
     public void deleteOutdatedMails(){
         var persistence = fakeSmtpConfigurationProperties.getPersistence();
         if(isDataRetentionConfigured(persistence)){
